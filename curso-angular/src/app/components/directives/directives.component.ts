1ec1018b2d335e6f44fd40fcc33789dee11b4483
D-Wolter <wolter.daniel@gmail.com>
2023-12-13 12:38:39 -0300

    Error occurs in the template of component DirectivesComponent.

---
+++ curso-angular/src/app/components/directives/directives.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  standalone: true,
+  imports: [],
+  templateUrl: './directives.component.html',
+  styleUrl: './directives.component.css'
+})
+export class DirectivesComponent {
+
+  size = 40;
+  
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+}
