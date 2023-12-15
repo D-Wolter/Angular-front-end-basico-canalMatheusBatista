4c6cfa2d763bf6d4871637787c1b79c775363641
D-Wolter <wolter.daniel@gmail.com>
2023-12-15 15:04:19 -0300

    intalacao json-server

--- curso-angular/src/app/app.component.ts
+++ curso-angular/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { IfRenderComponent } from "./components/if-render/if-render.component";
 import { EventosComponent } from "./components/eventos/eventos.component";
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
 
 import { EmitterComponent } from "./components/emitter/emitter.component";
 import { ListRenderComponent } from "./components/list-render/list-render.component";
@@ -19,7 +20,22 @@ import { TwoWayBindingComponent } from "./components/two-way-binding/two-way-bin
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [AppRoutingModule, FormsModule, CommonModule, RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent, EventosComponent, EmitterComponent, ListRenderComponent, PipesComponent, TwoWayBindingComponent]
+    imports: [
+      AppRoutingModule,
+      FormsModule,
+      CommonModule,
+      RouterOutlet,
+      FirstComponentComponent,
+      ParentDataComponent,
+      DirectivesComponent,
+      IfRenderComponent,
+      EventosComponent,
+      EmitterComponent,
+      ListRenderComponent,
+      PipesComponent,
+      TwoWayBindingComponent,
+      HttpClientModule
+    ]
 })
 export class AppComponent {
   userName = 'Dwolter';
